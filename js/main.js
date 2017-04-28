71a7c4734f2392ff38ad9e00c2b428b277f1f973
Peter Georgiev <peter_85@abv.bg>
2017-04-28 16:49:13 +0300

    Added Validation of signIn and signUp form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-$('body').ready(function () {
+import { validator } from 'validator';
+
+$('body').ready(function() {
     $('body').addClass('loaded');
 });
+
+
+// Jquery Validation signIn
+$('#signin-button').on('click', validator.validateSignIn);
+
+// Jquery Validation signUp
+$('#signup-button').on('click', validator.validateSignUp);
--- system-config.js
+++ system-config.js
@@ -5,7 +5,14 @@ SystemJS.config({
     map: {
         'plugin-babel': './node_modules/systemjs-plugin-babel/plugin-babel.js',
         'systemjs-babel-build': './node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
-        // app start script 
-        'main': './js/main.js'
+        // app scripts
+        'main': './js/main.js',
+        'validator': './js/helpers/validator.js',
+
+        // Library files
+        'jquery': 'https://code.jquery.com/jquery-3.2.1.min.js',
+        'handlebars': 'https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.6/handlebars.js',
+        'sammy': 'https://cdn.jsdelivr.net/sammy/0.7.4/sammy.js'
+
     }
 });
