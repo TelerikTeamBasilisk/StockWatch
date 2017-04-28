6ff61b6b07376e40c3c2e4b21a6c692de150fb46
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-04-28 17:38:46 +0300

    added database

---
+++ database/firebase-database.js
@@ -0,0 +1,44 @@
+import firebaseModule from 'firebase-config';
+
+//to be continued ...
+const firebaseDb = (function () {
+    const database = firebaseModule.database;
+    const auth = firebaseModule.auth;
+
+    function getChild(child) {
+        return database.child(child);
+    }
+
+    function createUserWithEmail(email, password, username) {
+        return auth.createUserWithEmailAndPassword(email, password)
+            .then(() => this.getCurrentUser())
+            .then(user => {
+                user.updateProfile({ displayName: username });
+            })
+            .catch(error => Promise.reject(error));
+    }
+
+    function signInWithEmail(email, password) {
+        return auth.signInWithEmailAndPassword(email, password)
+            .catch(error => Promise.reject(error));
+    }
+
+    function signOut() {
+        return auth.signOut();
+    }
+
+    function getCurrentUser() {
+        return new Promise(resolve => {
+            auth.onAuthStateChanged(userInfo => resolve(userInfo));
+        });
+    }
+
+    return {
+        createUserWithEmail,
+        signInWithEmail,
+        signOut,
+        getCurrentUser,
+    };
+} ());
+
+export default firebaseDb;
