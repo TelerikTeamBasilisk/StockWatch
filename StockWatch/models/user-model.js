d953b72bbd76dfcb0d1416daebe0f4732dee418e
EmiliyanPanagyurski <emiliyan.panagyurski@abv.bg>
2017-05-03 14:21:09 +0300

    add,remove from watchlist for user-model

--- StockWatch/models/user-model.js
+++ StockWatch/models/user-model.js
@@ -15,6 +15,26 @@ class UserModel {
         return firebaseDb.signOut()
             .catch(error => Promise.reject(error));
     }
+
+     addToWatchlist(company) {
+        return new Promise(resolve => {
+            const userId = localStorage.getItem('userUid');
+            firebaseDb.addToWatchlist(userId, company)
+                .then(data => resolve(data));
+        });
+    }
+
+    removeFromWatchlist(company) {
+        return new Promise(resolve => {
+            const userId = localStorage.getItem('userUid');
+            firebaseDb.removeFromWatchlist(userId, company)
+                .then(data => resolve(data));
+        });
+    }
+
+    getWatchlist() {
+    //TODO
+    }
 }
 
 const userModel = new UserModel();
