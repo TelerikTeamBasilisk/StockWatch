76d850b1757807d5cc6e8b604c586ccf238ec2b5
Milena Sapunova <milena.sapunova@gmail.com>
2017-05-07 14:46:57 +0300

    Stock data - real time quotes are working

--- StockWatch/database/stock-database.js
+++ StockWatch/database/stock-database.js
@@ -10,6 +10,20 @@ class StockData {
                 .fail(reject);
         });
     }
+
+    getStockQuote() {
+        let frequency = 'TIME_SERIES_INTRADAY';
+        let symbol = 'IBM';
+        let interval = '1min';
+
+        let base = `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D'http%3A%2F%2Fwww.alphavantage.co%2Fquery%3Ffunction%3D${frequency}%26symbol%3D${symbol}%26interval%3D${interval}%26apikey%3DAEU2'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
+
+        return new Promise((resolve, reject) => {
+            $.getJSON(base).
+            done(resolve).
+            fail(reject);
+        });
+    }
 }
 
 const stockData = new StockData();
